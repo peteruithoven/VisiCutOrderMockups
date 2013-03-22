799031a833bb0c1cd6894a865d9f4f57ac11361e
peteruithoven <peter@peteruithoven.nl>
2013-03-23 00:27:56 +0100

    first

---
+++ src/visicutordermockup/VisiCutOrderMockup.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package visicutordermockup;
+
+/**
+ *
+ * @author Peter
+ */
+public class VisiCutOrderMockup {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
